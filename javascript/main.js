449d3c01216cd0839b5fe100af77e17daf26704e
Hossam fahmi <hossameldinfahmi@gmail.com>
2023-01-29 20:56:18 +0200

    collsion

--- javascript/main.js
+++ javascript/main.js
@@ -282,11 +282,11 @@ function handleCollision() {
                     ball.x + ball.size > brick.x && // left brick side check
                     ball.x < brick.x + brick.w && // right brick side check
                     ball.y + ball.size > brick.y && // top brick side check
-                    ball.y < brick.y + brick.h &&  // bottom brick side check
-                    ball.y < brick.y + brick.w && // top of ball and right of brick
-                    ball.y + ball.size < brick.y + brick.w && // bottom of ball and right of brick
-                    ball.y > brick.y && // top of ball and left of brick
-                    ball.y + ball.size > brick.y  // bottom of ball and left of brick
+                    ball.y < brick.y + brick.h   // bottom brick side check
+                    // ball.y < brick.y + brick.w && // top of ball and right of brick
+                    // ball.y + ball.size < brick.y + brick.w && // bottom of ball and right of brick
+                    // ball.y > brick.y && // top of ball and left of brick
+                    // ball.y + ball.size > brick.y  // bottom of ball and left of brick
                 ) {
 
                      ball.dy *= -1;
